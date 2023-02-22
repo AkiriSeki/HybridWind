dd3d6ede7ef3f51a7f1fb955317717f8462c7cdd
Bret Bosma <bret.bosma@oregonstate.edu>
2023-02-22 09:41:35 -0800

    updated lynchpark_forcecontrol_v1b

--- utilities/init_hybrid_wind.m
+++ utilities/init_hybrid_wind.m
@@ -1,10 +1,15 @@
 addpath(genpath('/home/HybridWind/franka_matlab_v0.3.0'))
-addpath(genpath('/home/HybridWind/openfast/install/lib'))
-addpath(genpath('/home/HybridWind/openfast/glue-codes/simulink/src'))
-addpath(genpath('/home/HybridWind/runs'))
-addpath(genpath('/home/HybridWind/model'))
-addpath(genpath('/home/Akiri/FakeWind/SineWave'))
-addpath(genpath('/home/Akiri/FakeWind/FakeWind'))
+% addpath(genpath('/home/HybridWind/openfast/install/lib'))
+% addpath(genpath('/home/HybridWind/openfast/glue-codes/simulink/src'))
+% addpath(genpath('/home/HybridWind/runs'))
+% addpath(genpath('/home/HybridWind/model'))
+% addpath(genpath('/home/Akiri/FakeWind/SineWave'))
+% addpath(genpath('/home/Akiri/FakeWind/FakeWind'))
+addpath(genpath('/home/Akiri/HybridWind/openfast/install/lib'))
+addpath(genpath('/home/Akiri/HybridWind/openfast/glue-codes/simulink/src'))
+addpath(genpath('/home/Akiri/HybridWind/runs'))
+addpath(genpath('/home/Akiri/HybridWind/model'))
+addpath(genpath('usr/lib/x86_64-linux-gnu/lapack'))
 % makeinfo = make_slrt_config();
 init_franka_matlab();
 
